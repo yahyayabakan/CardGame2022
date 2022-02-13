40a875b9a78937a61b52ef6507af832be0d2fbdb
yahyayabakan <yahyaayabakan007@gmail.com>
2022-02-13 19:20:17 +0000

    abstract void spell method is added and all spell card classes will have to override this method in order to implement unique spell

--- app/structures/basic/Spell.java
+++ app/structures/basic/Spell.java
@@ -1,2 +1,8 @@
 public abstract class Spell extends Card{
+
+    /** All spell card classes implement Spell class and
+     * @override this method with their unique effect
+     * by default does nothing
+     */
+    public abstract void spell();
 }

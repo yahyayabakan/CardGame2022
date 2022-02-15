dbb5529d1f4ca15627ce9f3d1af37b1c0d1ff289
Ken Leung <kenlhk01@gmail.com>
2022-02-15 00:15:22 +0000

    mapped health and attack to unit

--- app/structures/units/Avatar.java
+++ app/structures/units/Avatar.java
@@ -3,5 +3,28 @@ package structures.units;
 import structures.basic.Unit;
 
 public class Avatar extends Unit {
+    private int health = 20;
+    private int attack = 2;
 
+    //override health & attack getters and setters as no BigCard is mapped for Avatar
+    @Override
+    public int getHealth() {
+        return health;
+    }
+
+    @Override
+    public void setHealth(int health) {
+
+        this.health = health;
+    }
+
+    @Override
+    public int getAttack() {
+        return attack;
+    }
+
+    @Override
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
 }

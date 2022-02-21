22868b53e52dddd90536d1b6f7789b0533553ce7
yahyayabakan <yahyaayabakan007@gmail.com>
2022-02-21 16:30:02 +0000

    Setting front end unit health after card is used

--- app/structures/spells/TrueStrike.java
+++ app/structures/spells/TrueStrike.java
@@ -13,8 +13,13 @@ public class TrueStrike extends Spell{
         //Deals 2 damage to enemy unit
         tile.getUnit().setHealth((tile.getUnit().getHealth()-2));
 
+        //Setting front end unit health
+        BasicCommands.setUnitHealth(out, tile.getUnit(), tile.getUnit().getHealth());
+
         //Truestrike animation
         BasicCommands.playEffectAnimation(out, BasicObjectBuilders.loadEffect(StaticConfFiles.f1_inmolation), tile);
         try {Thread.sleep(250);} catch (InterruptedException e) {e.printStackTrace();}
+
+
     }
 }

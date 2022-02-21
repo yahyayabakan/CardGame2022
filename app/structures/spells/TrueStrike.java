c254001b515e7855ee73648e923f7706805937fb
yahyayabakan <yahyaayabakan007@gmail.com>
2022-02-21 16:25:08 +0000

    Created class and spell method

---
+++ app/structures/spells/TrueStrike.java
@@ -0,0 +1,15 @@
+package structures.spells;
+
+import akka.actor.ActorRef;
+import commands.BasicCommands;
+import structures.GameState;
+import structures.basic.Spell;
+import structures.basic.Tile;
+import utils.BasicObjectBuilders;
+import utils.StaticConfFiles;
+
+public class TrueStrike extends Spell{
+    public void spell(ActorRef out, GameState gameState, Tile tile){
+       
+    }
+}

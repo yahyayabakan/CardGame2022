9399af6d6c74946a4e8713fbdfd66a19b574dfda
yahyayabakan <yahyaayabakan007@gmail.com>
2022-02-21 12:46:33 +0000

    Modified Spell class and spell() method

--- app/structures/basic/Spell.java
+++ app/structures/basic/Spell.java
@@ -1,10 +1,14 @@
 package structures.basic;
 
-public abstract class Spell extends Card{
+import akka.actor.ActorRef;
+import structures.GameState;
+
+public class Spell{
 
     /** All spell card classes implement Spell class and
      * @override this method with their unique effect
      * by default does nothing
      */
-    public abstract void spell();
+    public void spell(ActorRef out, GameState gameState, Tile tile){
+    }
 }

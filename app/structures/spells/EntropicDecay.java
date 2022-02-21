9cb6e9aae6adf3ff670c034a2b610b957268bd73
yahyayabakan <yahyaayabakan007@gmail.com>
2022-02-21 14:43:23 +0000

    Added entropic decay animation

--- app/structures/spells/EntropicDecay.java
+++ app/structures/spells/EntropicDecay.java
@@ -5,13 +5,19 @@ import commands.BasicCommands;
 import structures.GameState;
 import structures.basic.Tile;
 import structures.units.Avatar;
+import utils.BasicObjectBuilders;
+import utils.StaticConfFiles;
 
 public class EntropicDecay{
     public void spell(ActorRef out, GameState gameState, Tile tile){
         //First check whethet it is not an avatar
         //Then set the health to 0
         if(!(tile.getUnit() instanceof Avatar)){
-            tile.getUnit().setHealth(0);           
+            tile.getUnit().setHealth(0);     
+            
+            //Entropic Decay animation
+            BasicCommands.playEffectAnimation(out, BasicObjectBuilders.loadEffect(StaticConfFiles.f1_martyrdom), tile);
+            try {Thread.sleep(250);} catch (InterruptedException e) {e.printStackTrace();}
         }
     }
 }

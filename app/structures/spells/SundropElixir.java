8f0b1832ccc5f26b1862a57afce38d26b7b80365
yahyayabakan <yahyaayabakan007@gmail.com>
2022-02-21 13:03:43 +0000

    fixed compile issue

--- app/structures/spells/SundropElixir.java
+++ app/structures/spells/SundropElixir.java
@@ -14,20 +14,18 @@ import structures.basic.Unit;
 public class SundropElixir extends Spell{
     public void spell(ActorRef out, GameState gameState, Tile tile){
 
-    tile.getUnit().heal(5);
+        tile.getUnit().heal(5);
 
-    //Sets unit health in front end
-    BasicCommands.setUnitHealth(out, tile.getUnit(), tile.getUnit().getHealth());
+        //Sets unit health in front end
+        BasicCommands.setUnitHealth(out, tile.getUnit(), tile.getUnit().getHealth());
 
-    //if unit is an avatar then player health will be updated and mana front end will change
-    if(gameState.getPlayerOne().getPlayerNumber() == 1){
-        BasicCommands.setPlayer1Health(out, gameState.getPlayerOne());
-        BasicCommands.setPlayer1Mana(out, gameState.getPlayerOne());
-    }else{
-        BasicCommands.setPlayer2Health(out, gameState.getPlayerTwo());
-        BasicCommands.setPlayer1Mana(out, gameState.getPlayerTwo());
-    }
-
-    //Card delete and mana operations are handled by other methods
-    //Tested and working properly
-    }
+        //if unit is an avatar then player health will be updated and mana front end will change
+        if(gameState.getPlayerOne().getPlayerNumber() == 1){
+            BasicCommands.setPlayer1Health(out, gameState.getPlayerOne());
+            BasicCommands.setPlayer1Mana(out, gameState.getPlayerOne());
+        }else{
+            BasicCommands.setPlayer2Health(out, gameState.getPlayerTwo());
+            BasicCommands.setPlayer1Mana(out, gameState.getPlayerTwo());
+        }    
+    }   
+}

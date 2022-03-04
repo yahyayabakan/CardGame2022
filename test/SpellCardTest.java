9e5427ef8c4a6fa321e2925101fa79b33f591b9b
yahyayabakan <yahyaayabakan007@gmail.com>
2022-03-04 14:57:51 +0000

    Created SundropElixirTest

---
+++ test/SpellCardTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.json.Json;
+
+import actors.GameActor;
+import akka.actor.Actor;
+import akka.actor.ActorRef;
+import events.Initalize;
+import structures.GameState;
+import structures.basic.Tile;
+import structures.basic.Unit;
+import structures.spells.SundropElixir;
+import akka.actor.ActorRef;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import javax.validation.constraints.AssertTrue;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+
+
+public class SpellCardTest {
+    @Test
+    public void SundropElixirTest(){    
+        
+
+    }
+
+}
